b735f43523bf30549d1ce57497cd035ece5f6294
Vu Nguyen <vu_nguyen@epam.com>
2023-03-13 16:20:43 +0700

    fix: not realize locale paths

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'vi'],
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
 };
